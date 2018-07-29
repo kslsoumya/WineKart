2d635f7515a7ce736af9b092afc8d8266f203b89
kslsoumya <37289248+kslsoumya@users.noreply.github.com>
2018-07-29 17:37:27 +0530

    Add files via upload

--- api_project.js
+++ api_project.js
@@ -1,15 +1,15 @@
 define({
-  "name": "WineKart",
+  "name": "Group Chat Application",
   "version": "0.0.1",
-  "description": "Documentation for WineKart",
-  "title": "WineKart",
+  "description": "Documentation for Group Chat Application",
+  "title": "Group Chat Application",
   "url": "http://localhost:3000",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-07-28T17:45:41.704Z",
+    "time": "2018-07-29T11:52:45.891Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
